30377cbee213f05f692dae3d482c070bb86191c9
CharryNg <ziw255@ucsd.edu>
2016-12-26 04:06:24 -0800

    Refurbished the outlook of template.html Adding colors to the grid cells(tiles), and change the color of numbers to make them look less stimulating.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,75 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class Game extends Component {
-  render() {
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      cellNums: Array(16).fill(null),
+      highest: 0,
+      current: 0,
+      history: []
+    }
+    this.updateCellArr = this.updateCellArr().bind(this);
+  }
+
+  render()
+  {
+    return (<div className="game-wrapper">
+      <GameInfo highest={this.state.highest} current={this.state.current}/>
+      <GameGrid cellNums{this.state.cellNums} onKeyPress={this.keyHandler}/>
+    </div>);
+  }
+
+  keyHandler(event){
+
+  }
+
+  updateCellArr(cellArr)
+  {
+    this.setState({
+      cellNums: cellArr
+    });
+  }
+}
+
+class GameInfo extends Component {
+  constructor(props)
+  {
+    super(props);
+  }
+
+  render()
+  {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="game-upper">
+        <h1 className="game-title">2048</h1>
+        <div className="game-control">
+          <div className="score-tab">
+            <h3 className="score-tab-title">BEST</h3>
+            <div className="score-num">{this.props.highest}</div>
+          </div>
+          <div className="score-tab">
+            <h3 className="score-tab-title">SCORE</h3>
+            <div className="score-num">{this.props.current}</div>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
+  }
+}
+
+class GameGrid extends Component {
+  constructor(props)
+  {
+    super(props);
+  }
+
+  render()
+  {
+
   }
 }
 
